41efef65f659f3d8b7d284beb3c8c2b713e33135
LuAlcantara <alcantara.lucas@terra.com.br>
2023-11-14 10:59:05 -0300

    imersao-dev-14-11-23

---
+++ Aula1/main.js
@@ -0,0 +1,6 @@
+var nome = 'Lucas';
+var valorEmDolar = 30;
+var cotacaoDoDolar = 5.32;
+var valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+alert('Olá ' + nome + ' ' + 'o valor é ' + 'R$ ' + valorEmReal)
